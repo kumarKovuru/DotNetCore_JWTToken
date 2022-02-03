1d6330a89edc070dea804ed672be08c9fdbb5505
Kumar Kovuru <kumarkovuru@gmail.com>
2022-02-04 00:23:36 +0530

    Add project files.

---
+++ DotNetCore_JWTToken/Controllers/SecurityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace DotNetCore_JWTToken.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: SecurityController
+        public static string GenerateToken(string username)
+        {
+            byte[] key = Convert.FromBase64String("djkgutrutrhhhhfd33333333333333hfh435435jjnnn");
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(key);
+            SecurityTokenDescriptor descriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role, "Admin"),
+            }),
+                Expires = DateTime.UtcNow.AddMinutes(30),
+                SigningCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature)
+            };
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            JwtSecurityToken token = handler.CreateJwtSecurityToken(descriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}
